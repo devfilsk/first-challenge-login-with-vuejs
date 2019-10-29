e1e8fbab9b1478b1675904e441b85809a10bf67e
Filipe Maciel <devfilsk@gmail.com>
2019-10-29 00:12:12 -0300

    start to styled login view

--- src/web/modules/auth/pages/Login.vue
+++ src/web/modules/auth/pages/Login.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-      <h1>Login</h1>
+  <div class="login-page">
+    <div class="card">
+      <div class="card-header">Login</div>
+      <input type="text" class="form-control" placeholder="E-mail">
+      <input type="text" class="form-control" placeholder="Senha">
+    </div>
+      
   </div>
 </template>
 
@@ -11,5 +16,25 @@ export default {
 </script>
 
 <style>
-
+.login-page {
+  display: flex;
+  flex: 1;
+  background-color: #c5c5c5;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.card{
+  display: flex;
+  flex-direction: column;
+}
+.card input {
+  padding: 12px 12px;
+  margin-top: 20px;
+  border-radius: 4px;
+  width: 250px;
+}
+.card input:focus {
+  outline: none;
+}
 </style>
